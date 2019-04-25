4b718c6323ab3fd5ce06173d77ede057b97debda
Josh Dvir <josh@dvir.us>
2019-04-24 23:16:39 -0400

    adding logger of requests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
 )
 
 func main() {
@@ -13,7 +14,32 @@ func main() {
 		port = "9292"
 	}
 
-	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
+	customLogger := logger.New(logger.Config{
+		// Status displays status code
+		Status: true,
+		// IP displays request's remote address
+		IP: true,
+		// Method displays the http method
+		Method: true,
+		// Path displays the request path
+		Path: true,
+		// Query appends the url query to the Path.
+		Query: true,
+
+		Columns: true,
+
+		// if !empty then its contents derives from `ctx.Values().Get("logger_message")
+		// will be added to the logs.
+		MessageContextKeys: []string{"logger_message"},
+
+		// if !empty then its contents derives from `ctx.GetHeader("User-Agent")
+		MessageHeaderKeys: []string{"User-Agent"},
+	})
+
+	app.Use(customLogger)
+
+	app.OnAnyErrorCode(customLogger, func(ctx iris.Context) {
+	// app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
 		ctx.Writef("all go!")
 		ctx.StatusCode(200)
 		responseHeaders(ctx, "GET,POST")
